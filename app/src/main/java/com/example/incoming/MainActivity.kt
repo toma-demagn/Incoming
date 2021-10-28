a0bfe1e2618aa7f2f2452d51cadfd06628bde075
thomas <toma.demagn@gmail.com>
2021-10-28 17:57:22 +0200

    AJout du bouton

--- app/src/main/java/com/example/incoming/MainActivity.kt
+++ app/src/main/java/com/example/incoming/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.incoming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.incoming.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToLogin(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }

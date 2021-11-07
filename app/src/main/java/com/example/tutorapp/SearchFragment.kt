61be66de20e26b9d12286cabd89d8adcba7fb078
PabloMsnv <pabmaiso@live.fr>
2021-11-07 12:37:55 -0500

    Basic search feature in search fragment

--- app/src/main/java/com/example/tutorapp/SearchFragment.kt
+++ app/src/main/java/com/example/tutorapp/SearchFragment.kt
@@ -1,59 +1,50 @@
 package com.example.tutorapp
 
 import android.os.Bundle
+import android.view.*
+import android.widget.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var viewOfLayout: View
+    private lateinit var adapter: ArrayAdapter<*>
+
+    companion object {
+        fun newInstance(): SearchFragment {
+            return SearchFragment()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        viewOfLayout = inflater.inflate(R.layout.fragment_search, container, false)
+
+        // Get the layout views
+        val searchView = viewOfLayout.findViewById<SearchView>(R.id.sf_searchView)
+        val lv = viewOfLayout.findViewById<ListView>(R.id.sf_listView)
+        lv.emptyView = viewOfLayout.findViewById<TextView>(R.id.sf_emptyTextView)
+
+        // Init the adapter and pass it to the list view
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, resources.getStringArray(R.array.tags_list))
+        lv.adapter = adapter
+        lv.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            Toast.makeText(context, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        // Handle the search query
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return true
+            }
+        })
+
+        return viewOfLayout
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }

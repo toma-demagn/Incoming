2f804a2182919fff2f92414cccff3e29d1670945
PabloMsnv <pabmaiso@live.fr>
2021-11-07 23:09:07 -0500

    Search suggestions with simple JSON object

--- app/src/main/java/com/example/tutorapp/SearchFragment.kt
+++ app/src/main/java/com/example/tutorapp/SearchFragment.kt
@@ -1,12 +1,16 @@
 package com.example.tutorapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.*
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import org.json.JSONObject
 
 class SearchFragment : Fragment() {
+
+    private val jsonStringTags = """{"tag":"Mathématiques", "category":"C   ours"}"""
 
     private lateinit var viewOfLayout: View
     private lateinit var adapter: ArrayAdapter<*>
@@ -25,8 +29,12 @@ class SearchFragment : Fragment() {
         val lv = viewOfLayout.findViewById<ListView>(R.id.sf_listView)
         lv.emptyView = viewOfLayout.findViewById<TextView>(R.id.sf_emptyTextView)
 
+        // Get JSON object and adding the tags values in a list
+        val jsonObject = JSONObject(jsonStringTags)
+        val tagsList: MutableList<String> = mutableListOf(jsonObject.getString("tag"))
+
         // Init the adapter and pass it to the list view
-        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, resources.getStringArray(R.array.tags_list))
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, tagsList)
         lv.adapter = adapter
         lv.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
             Toast.makeText(context, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()

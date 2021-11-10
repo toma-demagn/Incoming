98517361a0935502589e17c378424de370ee95e1
justinepouchon <47656804+justinepouchon@users.noreply.github.com>
2021-11-10 10:18:40 -0500

    graphics improvement

---
+++ app/src/main/java/com/example/tutorapp/ShowAd.kt
@@ -0,0 +1,11 @@
+package com.example.tutorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowAd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_ad)
+    }
+}

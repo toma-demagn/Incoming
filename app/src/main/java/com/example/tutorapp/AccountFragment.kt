6c7207dab201a2dc97906615211732bbaef802f8
thomas <toma.demagn@gmail.com>
2021-11-10 22:25:13 +0100

    Reparation du design

--- app/src/main/java/com/example/tutorapp/AccountFragment.kt
+++ app/src/main/java/com/example/tutorapp/AccountFragment.kt
@@ -1,10 +1,14 @@
 package com.example.tutorapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +37,17 @@ class AccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        editButton.setOnClickListener{ goToEdit() }
+    }
+    fun goToEdit() {
+        val intent = Intent(context as Activity, editProfile::class.java)
+        startActivity(intent)
     }
 
     companion object {

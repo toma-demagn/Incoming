6cec0013aa6de98d018dc3f0630ba1cd5f7d8780
PabloMsnv <pabmaiso@live.fr>
2021-12-08 16:34:27 -0500

    Adjust account fragment with user data

--- app/src/main/java/com/example/tutorapp/ui/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/tutorapp/ui/fragments/AccountFragment.kt
@@ -2,74 +2,70 @@ package com.example.tutorapp.ui.fragments
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorapp.R
+import com.example.tutorapp.data.model.User
+import com.example.tutorapp.data.network.UserRetriever
+import com.example.tutorapp.data.utils.TimestampUtils
 import com.example.tutorapp.ui.activities.EditProfileActivity
 import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.coroutines.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val USER_ID = "userId"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var sp: SharedPreferences
+    private var userId: Int? = null
+    private val userRetriever: UserRetriever = UserRetriever()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        sp = this.requireActivity().getSharedPreferences("login", AppCompatActivity.MODE_PRIVATE)
+        userId = sp.getInt(USER_ID, -1)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        editButton.setOnClickListener{ goToEdit() }
+        getUserData()
+        accountFragment_editButton.setOnClickListener{ goToEdit() }
     }
 
-    fun goToEdit() {
+    private fun getUserData() {
+        val userFetchJob = Job()
+        val errorHandler = CoroutineExceptionHandler { _, throwable ->
+            throwable.printStackTrace()
+            Toast.makeText(context, "Impossible de récupérer les informations du profil...", Toast.LENGTH_SHORT)
+                .show()
+        }
+        val scope = CoroutineScope(userFetchJob + Dispatchers.Main)
+        scope.launch(errorHandler) {
+            val user = userRetriever.getUserById(userId!!)
+            initUIWithUserData(user)
+        }
+    }
+
+    private fun initUIWithUserData(user: User) {
+        accountFragment_usernameTextView.text = user.username
+        val names = "${user.firstName} ${user.lastName}"
+        accountFragment_namesTextView.text = names
+        accountFragment_emailTextView.text = user.email
+        accountFragment_birthdateTextView.text = TimestampUtils.timestampToDate(user.birthDate, true)
+    }
+
+    private fun goToEdit() {
         val intent = Intent(context as Activity, EditProfileActivity::class.java)
         startActivity(intent)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
